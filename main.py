bf32053908e6ed260f8f7c0ae7ac8acf4e96da6d
lc160518 <lc160518@ll.liemerscollege.nl>
2023-03-21 09:50:16 +0100

    list and reply

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 # bot.py
 import os
+import random
 
 import discord
 from dotenv import load_dotenv
@@ -23,6 +24,12 @@ async def on_ready():
 async def on_message(message):
     message.content = message.content.lower()
 
+    users = []
+
+    whitelist = [659666664346091530, 398769543482179585]
+    for i in range(0, len(whitelist)):
+        users.append(await client.fetch_user(whitelist[i]))
+
     if client.user == message.author:
         return
 
@@ -35,6 +42,21 @@ async def on_message(message):
 
         if not isinstance(message.channel, discord.DMChannel):
             await message.delete()
+
+    if message.content.startswith("ratio") and message.content.endswith("ratio"):
+        await message.channel.send(f"+ {pickratio(message, r)}")
+
+    if message.content.startswith("!suggest"):
+        julian = await client.fetch_user(398769543482179585)
+        await julian.send(message.content)
+
+    if message.content.startswith("!list"):
+        await message.channel.send(f"{r}")
+
+    if message.content.startswith("!add") and message.author in users:
+        r = get_list(message)
+        r.append(remove_suggest(message))
+        await message.channel.send(f"added")
 
 
 def remove_prefix(prefix):
@@ -58,6 +80,21 @@ def listToString(words):
 
     # return string
     return (str1.join(words))
+
+
+def remove_suggest(m):
+    g = list(m.content)
+    for i in range(0, 5):
+        g.remove(g[0])
+    return listToString(g)
+
+def get_list(m):
+    return ["bozo", "touch grass", "go outside", "get real", "we do a little trolling", "counter ratio", "10.20.11.123",
+         "boom beach lvl 100", f"common {m.author.name} W", "gangnam style", "¿Donde esta la biblioteca?"]
+
+def pickratio(m, r):
+    x = random.randrange(len(r))
+    return r[x]
 
 
 client.run(TOKEN)
